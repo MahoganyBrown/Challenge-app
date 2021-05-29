db60ac65b1bd9788541760ebebcae27d56887ca0
MahoganyBrown <84889702+MahoganyBrown@users.noreply.github.com>
2021-05-29 08:45:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 function addNumbers() {
-  
-let numbers = //input from button
+  var n1 = document.getElementById("num1").value;
+  var n2 = document.getElementById("num2").value;
+  var n3 = document.getElementById("num3").value;
+     
+  let numbers = [n1, n2, n3]
 
-let sortednumbers = numbers.sort((a, b) => a - b);
-  
-  
-  
-  
-  
-  
-  
+  let sortednumbers = numbers.sort((a, b) => a - b);
+  return sortednumbers  
 }
