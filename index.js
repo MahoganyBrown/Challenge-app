3e1d21b04f37f2d7efdd054160c5163a9fa5c08c
MahoganyBrown <84889702+MahoganyBrown@users.noreply.github.com>
2021-05-29 12:36:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ function addNumbers() {
   var n1 = document.getElementById("num1").value;
   var n2 = document.getElementById("num2").value;
   var n3 = document.getElementById("num3").value;
-     
+}   
   
-  var numArray = [n1, n2, n3];
-  numArray.sort(function(a, b) {
-  return a - b;
-
+let numbers = [n1, n2 , n3 ];
+numbers.sort( function( a , b){
+    if(a > b) return 1;
+    if(a < b) return -1;
+    return 0;
 });
 
-  console.log(numArray);
+console.log(numbers);
+
+
+
+
   
 
   
